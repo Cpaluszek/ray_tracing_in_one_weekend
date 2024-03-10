5de3f8cca7b2295c454cbeb8f58e2845492773a7
Clement <paluszek.clement@gmail.com>
2024-03-10 14:10:51 +0100

    add material class, and implement metalic and diffuse

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -5,7 +5,7 @@
 
 #include "color.h"
 #include "hittable.h"
-#include "vec3.h"
+#include "material.h"
 
 class camera {
 public:
@@ -80,7 +80,7 @@ private:
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray&r, int depth, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
 
         // If the number of ray bounce reach limit, no more light is gathered
@@ -89,8 +89,13 @@ private:
         }
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            ray scattered;
+            color attenuation;
+
+            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                return attenuation * ray_color(scattered, depth - 1, world);
+            }
+            return color(0, 0, 0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
